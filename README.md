5671b9339ec567948b1bd659959b17cf6ec3e533
jaschlitter@icloud.com <jaschlitter@icloud.com>
2020-05-30 11:58:52 -0700

    add mpl 2.0 license

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ab7105aeddc30d6eb7644b139b0bd30064fd69c0ed12d64dad571f19582bbf9
-size 220
+oid sha256:7d64723c95baaa555282348a2eb27ee86b195474237686a6e59d811bef2b1a9f
+size 509
--- js/randomgif.js
+++ js/randomgif.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e76d75e15d1a80ab72a73bf9cdd102c5f444a6b815de676d8a7460220d70068
-size 9206
+oid sha256:1b15ef7c409afc134f97341fff011c8c13f12963acd09943d2b2754ef71614d3
+size 9345
