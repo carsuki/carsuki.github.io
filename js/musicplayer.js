0c68be53476559288732265f4d89fd0118844ebd
jaschlitter@icloud.com <jaschlitter@icloud.com>
2020-06-13 14:10:18 -0700

    add music player

--- js/musicplayer.js
+++ js/musicplayer.js
@@ -1,15 +1,19 @@
-var cache = new LastFMCache();
+async function getData() {
+  const req = await fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=carsuki&api_key=105e1ad969864b7e7b019bd0bcdf2fc1&limit=1&nowplaying=true&format=json');
+  const { recenttracks } = await req.json();
 
-/* Create a LastFM object */
-var lastfm = new LastFM({
-  apiKey    : key,
-  apiSecret : secret,
-  cache     : cache
-});
+  console.log(recenttracks["track"][0]["name"]);
+  console.log(recenttracks["track"][0]["artist"]["#text"]);
+  console.log(recenttracks["track"][0]["album"]["#text"]);
 
-/* Load some artist info. */
-lastfm.artist.getInfo({artist: 'Kanye West'}, {success: function(data){
-  /* Use data. */
-}, error: function(code, message){
-  /* Show error message. */
-}});
+  document.getElementById("title").innerHTML = recenttracks["track"][0]["name"];
+  document.getElementById("artist").textContent = recenttracks["track"][0]["artist"]["#text"];
+  document.getElementById("album").textContent = recenttracks["track"][0]["album"]["#text"];
+  document.getElementById("cover").src = recenttracks["track"][0]["image"][3]["#text"];
+
+  if (!recenttracks.track || !recenttracks.track.length) {
+    console.log('No track available');
+    return;
+  }
+}
+getData();
