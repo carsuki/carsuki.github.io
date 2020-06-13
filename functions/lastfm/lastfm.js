755969a553a576c6904dcfcfdacdb0bfe384e55f
jaschlitter@icloud.com <jaschlitter@icloud.com>
2020-06-13 04:35:48 -0700

    first try at lastfm function

--- functions/lastfm/lastfm.js
+++ functions/lastfm/lastfm.js
@@ -1,26 +1,11 @@
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const key = process.env.API_KEY
+    const secret = process.env.API_SECRET
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      statusCode: 200
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
 }
-
-var cache = new LastFMCache();
-
-var lastfm = new LastFM({
-  apiKey    : key,
-  apiSecret : secret,
-  cache     : cache
-});
-
-lastfm.artist.getInfo({artist: 'The xx'}, {success: function(data){
-}, error: function(code, message){
-}});
