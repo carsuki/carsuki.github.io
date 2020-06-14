16cc390f1536044c9b7c4b5d89fa18504a9c071f
jaschlitter@icloud.com <jaschlitter@icloud.com>
2020-06-14 07:37:50 -0700

    add geocities style buttons to acknowledgements page, make modal smaller, wash folders

---
+++ js/eyeballtools.js
@@ -0,0 +1,41 @@
+// eyeballtools.js
+// misc. website functions
+// carsuki '20
+
+// eyeballPulsar
+function eyeballPulsar() {
+  document.getElementsByClassName("header-img")[0].setAttribute("class", "pulsing"); 
+}
+
+// eyeballChroma
+var clickState = 0;
+var chroma = document.getElementById('chroma');
+var image = document.getElementById('random');
+var source = document.getElementsByClassName("source-label")[0];
+var table = document.getElementsByClassName('sweeper-table');
+
+chroma.addEventListener('click', function() {
+
+  if (clickState == 0) {
+    image.src = 'img/gif/eyeball.gif';
+    source.innerHTML = 'Paloma Kop';
+    source.href='https://giphy.com/palomakop/';
+    document.getElementById('gif-container').style.border = "0px solid #000";
+    clickState = 1;
+  } else if (clickState == 1) {
+    eyeballTV();
+    clickState = 2;
+  }
+
+});
+
+// eyeballTV
+function eyeballTV() {
+  image.setAttribute("class", "shutdown");
+  setTimeout(() => { document.getElementById('gif-container').style.height = "0px";
+    document.getElementById('gif-container').parentNode.removeChild(document.getElementById('gif-container'));
+    source.innerHTML = "";
+    node.innerHTML = "???";
+    node.style["font-style"] = "italic"; 
+    node.style["text-decoration"] = "underline" }, 1600);
+}
--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,40 @@
 var modal = document.getElementsByClassName('modal');
-var openBtn = document.getElementById("modal-open");
-var closeBtn = document.getElementsByClassName("modal-close")[0];
+var btn = document.getElementsByClassName("opener");
+var close = document.getElementsByClassName("close");
 
-openBtn.onclick = function() {
-  modal[0].style.display = "block";
-}
+var node = document.createElement("A");
+var textnode = document.createTextNode("a");
+var label = document.getElementsByClassName('source-label');
+node.appendChild(textnode)
+document.getElementById("label-span").appendChild(node);
+node.setAttribute("id", "opener2");
+node.classList.add("opener");
+node.innerHTML = "";
 
-closeBtn.onclick = function() {
-  modal[0].style.display = "none";
-}
+// document.getElementById('opener3').parentNode.removeChild(document.getElementById('opener3'));
 
-window.onclick = function(event) {
-  if (event.target == modal[0]) {
-    modal[0].style.display = "none";
+function eyeballModal() {
+  btn[0].onclick = function() {
+    modal[0].style.display = "block";
+  }
+
+  btn[1].onclick = function() {
+      modal[1].style.display = "block";
+  }
+
+  close[0].onclick = function() {
+      modal[0].style.display = "none";
+  }
+
+  close[1].onclick = function() {
+      modal[1].style.display = "none";
+  }
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
   }
 }
+
+eyeballModal();
