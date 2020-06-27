f89fa87e4ea53fd000e0d21f038fbfdb536ca11b
jaschlitter@icloud.com <jaschlitter@icloud.com>
2020-06-27 14:20:44 -0700

    add new /gif page

--- js/musicplayer.js
+++ js/musicplayer.js
@@ -2,6 +2,7 @@ async function getData() {
   const req = await fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=carsuki&api_key=105e1ad969864b7e7b019bd0bcdf2fc1&limit=1&nowplaying=true&format=json');
   const { recenttracks } = await req.json();
   console.log(recenttracks);
+  console.log("^^^ lol look at how messy this data is ^^^")
 
   document.getElementById("title").innerHTML = recenttracks["track"][0]["name"];
   document.getElementById("artist").textContent = recenttracks["track"][0]["artist"]["#text"];
--- js/shufflegifs.js
+++ js/shufflegifs.js
@@ -14,5 +14,31 @@ function shuffle(array) {
   return array;
 }
 
-shuffle(arr);
-console.log(arr);
+shuffle(randomgifs);
+
+for (let step = 1; step < 78; step++) {
+  var contain = document.getElementById("container")
+  var containLink = document.createElement("a");
+  contain.appendChild(containLink);
+
+  var gif = document.createElement("img");
+  gif.setAttribute("id", step); 
+  containLink.appendChild(gif)
+
+  var child = document.getElementById(step);
+  var id = step - 1;
+
+  child.src = '../' + randomgifs[id].gif;
+
+  child.parentElement.href = randomgifs[id].link;
+
+  var cap = document.createElement("p");
+  cap.className = "caption";
+
+  cap.innerHTML = randomgifs[id].name;
+
+  child.parentElement.appendChild(cap);
+}
+
+var amount = document.querySelectorAll('a').length;
+console.log(amount + ' images loaded')
