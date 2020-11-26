fe98bff91e9c743539252087c61f485dbb1db190
jaschlitter@icloud.com <jaschlitter@icloud.com>
2020-11-25 21:52:30 -0800

    Revert "update lastfm"
    This reverts commit fee2f9423e42940672683e9407a349e9654b19b7.

--- js/musicplayer.js
+++ js/musicplayer.js
@@ -1,5 +1,5 @@
 async function getData() {
-  const req = await fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=yolk&api_key=2cb62afd1279a0fe57c4b69601ac8aa9&limit=1&nowplaying=true&format=json');
+  const req = await fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=carsuki&api_key=105e1ad969864b7e7b019bd0bcdf2fc1&limit=1&nowplaying=true&format=json');
   const { recenttracks } = await req.json();
   console.log(recenttracks);
   console.log("^^^ lol look at how messy this data is ^^^")
