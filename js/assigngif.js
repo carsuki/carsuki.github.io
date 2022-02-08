2788df3e3cfad9a9a575417e4aa9f05472409f09
jaschlitter@icloud.com <jaschlitter@icloud.com>
2022-02-08 08:16:54 -0800

    old site is back in town

--- js/assigngif.js
+++ js/assigngif.js
@@ -3,11 +3,10 @@
 // carsuki '20
 
 // select random gif
-var randomset = randomgifs[Math.floor(Math.random() * randomgifs.length)];
-console.log(randomset); 
+var randomset = randomgifs[Math.floor(Math.random() * randomgifs.length)]; 
 
 // assign the selected gif to the image element on the page
-document.querySelector('#random').src = randomset.gif;
+document.getElementById('random').src = randomset.gif;
 
 // fill in the source text
 document.getElementsByClassName("source-label")[0].innerHTML = randomset.name;
