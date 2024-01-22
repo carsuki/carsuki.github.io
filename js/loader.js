f02b7d6f404a4508dc069b9c6b5f8bd19fc84f5a
carsuki <hello@carsuki.moe>
2024-01-22 01:45:15 -0800

    loader: wait for window.onload again but force load after 20 sec

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,12 @@
-window.addEventListener('DOMContentLoaded',function() {
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+window.onload = function() {
   document.getElementById("loading").style.display = "none";
-});
+};
+
+async function fallbackLoad() {
+  await sleep(20000);
+  document.getElementById("loading").style.display = "none";
+}
+
+fallbackLoad();
